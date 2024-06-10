10ce182c34b7a495c2c8b1af0de4cfa36dce8526
awebstudent <126592792+awebstudent@users.noreply.github.com>
2024-06-10 13:33:44 +0530

    added commenet in authService.ts

--- Week05_Assignment_Soham_Dalvi/src/services/authService.ts
+++ Week05_Assignment_Soham_Dalvi/src/services/authService.ts
@@ -4,15 +4,15 @@ import Employee from '../models/employeeModel';
 import credentials from '../common/credentials';
 
 export async function registerEmployee (name: string, email: string, password: string)  {
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10); //used bcrypt for password hashing
   
-  const employee = await Employee.create({ name, email, password: hashedPassword, assignedShiftHours: 8, role: 'Employee' });
+  const employee = await Employee.create({ name, email, password: hashedPassword, assignedShiftHours: 8, role });
   return employee;
 };
 
 export async function loginEmployee  (email: string, password: string)  {
   const employee = await Employee.findOne({ where: { email } });
-  if (!employee || !await bcrypt.compare(password, employee.password)) {
+  if (!employee || !await bcrypt.compare(password, employee.password)) {  
     throw new Error('Invalid credentials');
   }
   const token = jwt.sign({ id: employee.id, role: employee.role }, credentials.jwt.SECRET, { expiresIn: '10h' });
